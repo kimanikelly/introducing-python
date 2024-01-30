0e17a74753b5343f0ce08f88a797968f217b0c3b
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2024-01-29 21:27:57 -0500

    Changes that were not included

---
+++ 00_types_values_variables_names/things_to_do/assignment.py
@@ -0,0 +1,20 @@
+# Assign the integer value 99 to the variable 'prince', and print it
+prince = 99
+print("The value of prince is:", prince)
+
+# What type is the value 5
+# 5 is an int type
+five = 5
+five_type = type(five)
+print(five_type)
+
+# What type is the value 2.0
+# 2.0 is a float type
+two_point_o = 2.0
+print(type(two_point_o))
+
+# What type is the expression 5 + 2.0
+# 5 + 2.0 is a float type
+sum_result = five + two_point_o
+result_type = type(sum_result)
+print(result_type)
